130cd473a325a3ee2062e10c65dc24a7dc2d66c9
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-06-27 16:06:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+'use strict';
+var gutil = require('gulp-util'),
+  PJV = require('package-json-validator').PJV,
+  through = require('through2');
+
+function printErrors(results) {
+  if (results) {
+
+    if (results.critical) {
+      gutil.log(gutil.colors.red('Critical Error: ' + results.critical));
+    }
+
+    if (results.errors) {
+      gutil.log(gutil.colors.underline.red('Errors:'));
+
+      results.errors.forEach(function (error) {
+        gutil.log('  ' + gutil.colors.red(error));
+      });
+    }
+
+    if (results.warnings) {
+      gutil.log(gutil.colors.underline.yellow('Warnings:'));
+
+      results.warnings.forEach(function (warn) {
+        gutil.log('  ' + gutil.colors.yellow(warn));
+      });
+    }
+
+    if (results.recommendations) {
+      gutil.log(gutil.colors.underline('Recommendations:'));
+
+      results.recommendations.forEach(function (rec) {
+        gutil.log('  ' + rec);
+      });
+    }
+
+    if (results.valid) {
+      gutil.log(gutil.colors.green('package.json is valid'));
+    } else {
+      gutil.log(gutil.colors.red('package.json is INVALID'));
+      gutil.beep();
+    }
+
+
+  } else {
+    gutil.beep();
+    throw new gutil.PluginError('gulp-nice-package', 'Failed to get results from validator');
+  }
+}
+
+module.exports = function (options) {
+
+  function validate(file, enc, cb) {
+
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('gulp-nice-package', 'Streaming not supported'));
+      return cb();
+    }
+
+    var results = PJV.validate(file.contents.toString(), null /*defaults to npm*/, options);
+
+    printErrors.call(this, results);
+
+    file.nicePackage = results;
+
+    this.push(file);
+    cb();
+  }
+
+  return through.obj(validate);
+};
